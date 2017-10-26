621f43cf3e6de339e4f0863ff1c35e9ca4bfbaa4
Марк Трясцин <Mark@MarkBook-Air.local>
2017-10-26 17:57:52 +0300

    dpad

--- Tank/GameScene.swift
+++ Tank/GameScene.swift
@@ -10,26 +10,62 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
+    var moving: Bool = false
+    var touch: UITouch?
+    var tlx, dx: CGFloat?
+    var tly, dy: CGFloat?
+    var yzero: CGFloat = 0
+    var xzero: CGFloat = 0
     var entities = [GKEntity]()
     var graphs = [String : GKGraph]()
     
     private var lastUpdateTime : TimeInterval = 0
     private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var player: SKSpriteNode?
     
     override func sceneDidLoad() {
-}
+        player = childNode(withName: "TankPlayer") as? SKSpriteNode
+        
+    }
  
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if moving == false {
+            touch = touches.first
+            moving = true
+            xzero = (touch?.location(in: self).x)!
+            yzero = (touch?.location(in: self).y)!
+        }
+    }
     
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if touch == touches.first {
+            tlx = touches.first?.location(in: self).x
+            tly = touches.first?.location(in: self).y
+            dx = (touches.first?.location(in: self).x)! - xzero
+            dy = (touches.first?.location(in: self).y)! - yzero
+            if tlx! > xzero && abs(dx!) > abs(dy!) {
+                player?.physicsBody?.velocity = CGVector(dx: 200, dy: 0)
+            } else if tlx! < xzero && abs(dx!) > abs(dy!) {
+                player?.physicsBody?.velocity = CGVector(dx: -200, dy: 0)
+            } else if tly! > yzero && abs(dy!) > abs(dx!) {
+                player?.physicsBody?.velocity = CGVector(dx: 0, dy: 200)
+            } else if tly! < yzero && abs(dy!) > abs(dx!) {
+                player?.physicsBody?.velocity = CGVector(dx: 0, dy: -200)
+            }
+        }
+    }
     
-    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if touch == touches.first {
+            moving = false
+            player?.physicsBody?.velocity = CGVector (dx: 0, dy: 0)
+        }
+    }
    
     
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
-        
         // Initialize _lastUpdateTime if it has not already been
         if (self.lastUpdateTime == 0) {
             self.lastUpdateTime = currentTime
